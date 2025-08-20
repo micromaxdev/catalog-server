376dddaf7032fc7d3ed5c695f0d70932a56f7ee5
Andysouza24 <adsouza@micromax.com.au>
2025-08-20 13:33:30 +1000

    Changes to load all the Data from the Database

--- controllers/productController.js
+++ controllers/productController.js
@@ -22,11 +22,9 @@ export const getAllProducts = async (req, res) => {
       replacements.description = `%${description}%`;
     }
 
-    // Get all products with S3 URLs
-    const [results] = await sequelize.query(
-      `SELECT * FROM product ${whereClause} LIMIT 100`,
-      { replacements }
-    );
+    const [results] = await sequelize.query(`SELECT * FROM product`, {
+      replacements,
+    });
 
     // Transform the results to match frontend expectations
     const transformedResults = results.map((product) => ({
@@ -47,27 +45,8 @@ export const getAllProducts = async (req, res) => {
         new Date().toISOString(),
     }));
 
-    console.log(`Found ${transformedResults.length} products`);
-
-    // Log sample of products with images/datasheets for debugging
-    const productsWithAssets = transformedResults.filter(
-      (p) => p.image_path || p.datasheet_path
-    );
-    if (productsWithAssets.length > 0) {
-      console.log(`Products with assets: ${productsWithAssets.length}`);
-      console.log("Sample product with assets:", {
-        model: productsWithAssets[0].model_number,
-        hasImage: !!productsWithAssets[0].image_path,
-        hasDatasheet: !!productsWithAssets[0].datasheet_path,
-        imageUrl: productsWithAssets[0].image_path?.substring(0, 50) + "...",
-        datasheetUrl:
-          productsWithAssets[0].datasheet_path?.substring(0, 50) + "...",
-      });
-    }
-
     res.status(200).json(transformedResults);
   } catch (err) {
-    console.error("Database error:", err);
     res.status(500).json({ error: err.message });
   }
 };
